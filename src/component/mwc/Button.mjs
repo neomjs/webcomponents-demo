d83b30a8aa4324d025f1e483246242d0ca084733
tobiu <tobiasuhlig78@gmail.com>
2021-08-10 14:42:23 +0200

    component.mwc.Button: ntype, doc comments

--- src/component/mwc/Button.mjs
+++ src/component/mwc/Button.mjs
@@ -6,7 +6,19 @@ import Component from '../../../node_modules/neo.mjs/src/component/Base.mjs';
  */
 class Button extends Component {
     static getConfig() {return {
+        /**
+         * @member {String} className='Neo.component.mwc.Button'
+         * @protected
+         */
         className: 'Neo.component.mwc.Button',
+        /**
+         * @member {String} ntype='mwc-button'
+         * @protected
+         */
+        ntype: 'mwc-button',
+        /**
+         * @member {Object} _vdom
+         */
         _vdom:
         {tag: 'mwc-button', label: 'Hello World'}
     }}
