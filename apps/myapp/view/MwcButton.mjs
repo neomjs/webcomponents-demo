7573353b11967ff9e214a48c40b607b328ff3c54
tobiu <tobiasuhlig78@gmail.com>
2021-08-10 13:01:52 +0200

    MyApp.view.MwcButton: base class

---
+++ apps/myapp/view/MwcButton.mjs
@@ -0,0 +1,17 @@
+import Component from '../../../node_modules/neo.mjs/src/component/Base.mjs';
+
+/**
+ * @class MyApp.view.MwcButton
+ * @extends Neo.component.Base
+ */
+class MwcButton extends Component {
+    static getConfig() {return {
+        className: 'MyApp.view.MwcButton',
+        _vdom:
+        {tag: 'mwc-button'}
+    }}
+}
+
+Neo.applyClassConfig(MwcButton);
+
+export {MwcButton as default};
